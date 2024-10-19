c6308c1b25ef636ca01be88d0be13630109d2d13
an3z4m <kerkacha.nabil@gmail.com>
2024-10-19 19:25:51 +0200

    Set up Composer for development dependencies and integrate PHPUnit Polyfills

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,10 @@
  * @package Cool_Kids_Network
  */
 
+ // Load the Composer autoloader to include PHPUnit Polyfills
+require_once dirname( __FILE__ ) . '/../vendor/autoload.php';
+
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
